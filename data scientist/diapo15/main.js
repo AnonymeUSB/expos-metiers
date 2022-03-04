1fe4d6b87043fcb3fbb386d9e17229a421135b87
AnonymeUSB <Allyssisation@gmail.com>
2022-03-04 06:41:58 +0000

    25

---
+++ data scientist/diapo15/main.js
@@ -0,0 +1,11 @@
+function droite() {
+    window.open("../diapo16/index.html", "_self");
+}
+
+function bas() {
+    window.open("../../index.html", "_self");
+}
+
+function gauche() {
+    window.open("../diapo14/index.html", "_self");
+}
