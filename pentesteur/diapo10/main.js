bd5b400ae8e5eef64555038da99e7ae5b665ec1a
AnonymeUSB <Allyssisation@gmail.com>
2022-03-04 15:49:31 +0000

    38

---
+++ pentesteur/diapo10/main.js
@@ -0,0 +1,11 @@
+function droite() {
+    window.open("../diapo11/index.html", "_self");
+}
+
+function bas() {
+    window.open("../../index.html", "_self");
+}
+
+function gauche() {
+    window.open("../diapo9/index.html", "_self");
+}
